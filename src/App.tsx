634f00914af557d329b414400f69c10fbf9d65af
Edian Comachio <comachio.edian@gmail.com>
2024-09-02 23:00:47 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,126 @@
+import React, { useState, useEffect } from "react";
+import gasStations from "./gasStations";
+
+interface GasStation {
+  name: string;
+  latitude: number;
+  longitude: number;
+  url: string;
+}
+
+interface Location {
+  lat: number;
+  lng: number;
+}
+
+const App: React.FC = () => {
+  const [userLocation, setUserLocation] = useState<Location | null>(null);
+  const [closestLocation, setClosestLocation] = useState<GasStation | null>(
+    null
+  );
+
+  // Function to calculate distance using Haversine formula
+  const calculateDistance = (
+    lat1: number,
+    lon1: number,
+    lat2: number,
+    lon2: number
+  ): number => {
+    const R = 6371; // Radius of the Earth in km
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLon / 2) +
+      Math.cos(lat1 * (Math.PI / 180)) *
+        Math.cos(lat2 * (Math.PI / 180)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c; // Distance in km
+    return distance;
+  };
+
+  // Function to find the closest gas station
+  const findClosestGasStation = (userLat: number, userLng: number) => {
+    let closest: GasStation | null = null;
+    let minDistance = Infinity;
+
+    gasStations.forEach((station) => {
+      const distance = calculateDistance(
+        userLat,
+        userLng,
+        station.latitude,
+        station.longitude
+      );
+      if (distance < minDistance) {
+        minDistance = distance;
+        closest = station;
+      }
+    });
+
+    setClosestLocation(closest);
+  };
+
+  // Get user's current location
+  useEffect(() => {
+    const getLocation = async () => {
+      await navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ lat: latitude, lng: longitude });
+          findClosestGasStation(latitude, longitude);
+        },
+        (error) => {
+          console.error("Error getting location:", error);
+        }
+      );
+    };
+    getLocation();
+  }, []);
+
+  // Generate Google Maps URL
+  const getGoogleMapsLink = (lat: number, lng: number): string => {
+    return `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`;
+  };
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center w-full">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs_XuIIHCtBbXCt3G_1g7ca5Z45TWOyfUKtw&s"
+        alt="Logo"
+        className="mb-4"
+      />
+
+      <div className=" bg-gray-100 flex items-center justify-center">
+        <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
+          <h3 className="text-2xl font-bold text-center text-blue-600 mb-6 flex flex-col items-center">
+            Posto SIM mais perto kkkkkkk
+          </h3>
+          {closestLocation ? (
+            <div className="text-center">
+              <p>O posto SIM mais próximo é:</p>
+              <p className="text-lg font-semibold mb-4">
+                {closestLocation.name}
+              </p>
+              <a
+                href={closestLocation.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block"
+              >
+                <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105">
+                  Open in Google Maps
+                </button>
+              </a>
+            </div>
+          ) : (
+            <p className="text-gray-600 text-center">
+              Achando posto SIM mais próximo...
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/gasStations.ts
@@ -0,0 +1,91 @@
+interface GasStation {
+  name: string;
+  latitude: number;
+  longitude: number;
+  url: string;
+}
+
+const gasStations: GasStation[] = [
+  {
+    name: "Posto SIM - Cinquentenário",
+    latitude: -29.17423743774277,
+    longitude: -51.2142520220287,
+    url: "https://maps.app.goo.gl/eNhHLqiZNSkUYjJ28",
+  },
+  {
+    name: "Posto SIM Cinquentenário - GNV - Petrobras",
+    latitude: -29.169441571565482,
+    longitude: -51.195463649008204,
+    url: "https://maps.app.goo.gl/uiCoGadg8Sig8YqXA",
+  },
+
+  {
+    name: "Posto SIM | São Leopoldo",
+    latitude: -29.187373203279986,
+    longitude: -51.18185107599607,
+    url: "https://maps.app.goo.gl/UwJQx7itXm7wUeRD6",
+  },
+  {
+    name: "Posto Centenário - SIM Rede de Postos",
+    latitude: -29.165694169115703,
+    longitude: -51.19600429985051,
+    url: "https://maps.app.goo.gl/AkiouPBKYGysVZEd8",
+  },
+  {
+    name: "Posto SIM | Floresta",
+    latitude: -29.173620999999997,
+    longitude: -51.1614146,
+    url: "https://maps.app.goo.gl/AgFR5DvFT6Vr3jDs6",
+  },
+  {
+    name: "Posto SIM | Matteo",
+    latitude: -29.159810538605413,
+    longitude: -51.19332209101178,
+    url: "https://maps.app.goo.gl/PX1B86CpXGxtma7A7",
+  },
+  {
+    name: "Posto SIM | Parque Do Sol",
+    latitude: -29.17560914687067,
+    longitude: -51.182207810935424,
+    url: "https://maps.app.goo.gl/LWhWYiy7ytTJB3yGA",
+  },
+  {
+    name: "Posto SIM | Cidade Nova",
+    latitude: -29.17492094561424,
+    longitude: -51.235075487081,
+    url: "https://maps.app.goo.gl/72muNo3vWytNdBe56",
+  },
+  {
+    name: "Posto SIM - Caxias Bela Vista",
+    latitude: -29.173267149844744,
+    longitude: -51.1507221379233,
+    url: "https://maps.app.goo.gl/fCxQ7Jm6NtJd9zJG6",
+  },
+  {
+    name: "Posto SIM | Av. França",
+    latitude:-29.184433053849347, 
+    longitude: -51.14997111944845,
+    url: "https://maps.app.goo.gl/NaU5MAr3YZ4PNiYn8"
+  },
+  {
+    name: "Posto SIM | Lourdes",
+    latitude: -29.173360828589946,
+    longitude:  -51.15943395223157,
+    url:"https://maps.app.goo.gl/3ZbvWnQLyTPGPHak9"
+  },
+  {
+    name: "Posto SIM | Nossa Senhora de Fátima",
+    latitude: -29.135395247339947,
+    longitude:  -51.18264295695968,
+    url: "https://maps.app.goo.gl/1cHwHBwPkz6jUcmJA"
+  },
+  {
+    name: "Posto SIM | 20 de Setembro",
+    latitude: -29.164113589478113, 
+    longitude: -51.1695210998505,
+    url: "https://maps.app.goo.gl/dU8Ma4DpExko7ZUMA"
+  }
+
+];
+
+export default gasStations;
